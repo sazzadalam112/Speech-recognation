dbc2ba8c0f49e645c85bc916bcb126e35e998c51
GeekySazzad <71886049+sazzadalam112@users.noreply.github.com>
2023-07-31 16:14:23 +0530

    spelling mistake

--- script.js
+++ script.js
@@ -1,34 +1,38 @@
-const SpeechRecognition = 
-window.SpeechRecognition || window.webkitSpeechRecognition;
-
+const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
 const rec = new SpeechRecognition();
 
 rec.lang = 'en-US';
-rec.continous = true;
+rec.continuous = true;
 
-rec.onresult = function (e) {
+rec.onresult = function(e) {
     const acceptedColors = [
-       'red',
-       'blue',
-       'green',
-       'yellow',
-       'pink',
-       'brown',
-       'purple',
-       'orange',
-       'black',
-       'white', 
-    ]
-    for(let i =e.resultIndex; i< e.results.length; i++) {
-        const script = e.results[i][0].transcript.toLowerCase().trim();
-        
-        if(acceptedColors.includes(script)){
-            document.body.style.backgroundColor = script;
-        }else {
-            alert('please  say a color')
-        }
+        'red',
+        'blue',
+        'green',
+        'yellow',
+        'pink',
+        'brown',
+        'purple',
+        'orange',
+        'black',
+        'white',
+      ];
+    for(let i = e.resultIndex; i < e.results.length;i++) {
+        const script = e.results[i][0].transcript;
+       if(acceptedColors.includes(script)) {
+        document.body.style.backgroundColor = script; 
+       } else{
+        alert('please say a color');
+       }
     }
+    // console.log(e.results[0][0].transcript);
 };
-
 rec.start();
+
+
+
+
+
+
+
 
